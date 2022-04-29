f5f5b9b5388afb92a7b5d865876c4fd321fd0f4e
Andreas Gebhardt <agebhar1@googlemail.com>
2022-04-29 19:52:40 +0200

    use »ESLint«
    ```
    $ npm install --save-dev --save-exact \
        eslint \
        eslint-plugin-vue \
        @typescript-eslint/parser \
        @typescript-eslint/eslint-plugin \
        @vue/eslint-config-typescript
    ```
    
    https://eslint.vuejs.org/user-guide/

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
+  ],
+  parser: 'vue-eslint-parser'
+}
