f62044e197fe06845922d3bc6df41763c22aa23a
Andreas Gebhardt <agebhar1@googlemail.com>
2022-04-29 20:56:56 +0200

    use »Prettier«
    ```
    $ npm install --save-dev --save-exact \
        prettier \
        eslint-plugin-prettier \
        @rushstack/eslint-patch \
        @vue/eslint-config-prettier
    ```

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,9 +1,17 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/eslint-config-prettier',
+    'plugin:prettier/recommended',
   ],
-  parser: 'vue-eslint-parser'
+  parser: 'vue-eslint-parser',
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
 }
